7b61f1b361f58911a85e43463e2600c8a19bed91
Adam Stockermans <adamstox@gmail.com>
2020-06-19 21:21:49 -0400

    Add wolves notes

--- src/FluteContainer.tsx
+++ src/FluteContainer.tsx
@@ -3,6 +3,11 @@ import TaggedInput from "./TaggedInput";
 
 import "./FluteContainer.sass";
 // import Notes from "./Notes";
+const wolves = [
+  ["F#4", "B4", "A#4", "B4", "A#4", "F#4", "D#4"],
+  ["D#5", "F#5", "B4", "A#4", "B4", "D#5", "G5", "D#5"],
+  ["F#5", "F#5", "D#5", "-", "D5", "F4"],
+];
 
 const FluteContainer = () => {
   const [notes, setNotes] = useState([[]] as Note[][]);
